5d339c0da7e2e1d2e646540e81777173da675542
Kun Chen <chenkun@mail.ustc.edu.cn>
2020-05-04 16:03:25 -0400

    benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -123,10 +123,10 @@ void MonteCarlo() {
   LOG_INFO("Loading Weight...")
   Markov.Weight.LoadFile();
 
-  // for (int order = 1; order <= Para.Order; ++order) {
-  //   Markov.Weight.Test(1);
-  //   Markov.Weight.Benchmark(order, 1000);
-  // }
+  for (int order = 1; order <= Para.Order; ++order) {
+    // Markov.Weight.Test(1);
+    Markov.Weight.Benchmark(order, 1000);
+  }
 
   while (true) {
     Block++;
