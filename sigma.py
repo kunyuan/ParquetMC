71ec33e9646a3e5f27223422155ba6915171975c
Kun Chen <chenkun@mail.ustc.edu.cn>
2020-05-05 12:49:52 -0400

    debugging sigma

--- sigma.py
+++ sigma.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import integrate
+from utility import *
 import matplotlib.pyplot as plt
 import matplotlib as mat
 import sys
@@ -10,136 +11,46 @@ mat.rcParams.update({'font.size': 16})
 mat.rcParams["font.family"] = "Times New Roman"
 size = 12
 
-# XType = "Tau"
-XType = "Mom"
+XType = "Tau"
+# XType = "Mom"
 # XType = "Z"
 OrderByOrder = False
-# 0: I, 1: T, 2: U, 3: S
-Order = [0, 1, 2, 3, 4, 5]
 SpinIndex = 2
+Dim = 3
+# 0: I, 1: T, 2: U, 3: S
 
-MaxOrder = None
-rs = None
-Lambda = None
-Mass2 = None
-Beta = None
-Charge2 = None
-TotalStep = None
-BetaStr = None
-rsStr = None
-ChargeStr = None
-LambdaStr = None
-Mass2Str = None
-
-with open("inlist", "r") as file:
-    line = file.readline()
-    para = line.split(" ")
-    MaxOrder = int(para[0])
-    BetaStr = para[1]
-    Beta = float(BetaStr)
-    rsStr = para[2]
-    rs = float(rsStr)
-    Mass2Str = para[3]
-    Mass2 = float(Mass2Str)
-    LambdaStr = para[4]
-    Lambda = float(LambdaStr)
-    ChargeStr = para[5]
-    Charge2 = float(ChargeStr)
-    TotalStep = float(para[6])
-
-Order = range(0, MaxOrder+1)
-
-folder = "./Beta{0}_rs{1}_lambda{2}/".format(int(Beta), rs, Mass2)
-# folder = "./3_Beta{0}_lambda{2}/".format(Beta, rs, Lambda)
-
-##############   2D    ##################################
-###### Bare Green's function    #########################
-# kF = np.sqrt(2.0)/rs  # 2D
-# Bubble=0.11635  #2D, Beta=0.5, rs=1
-# Bubble = 0.15916/2  # 2D, Beta=10, rs=1
-# Bubble = 0.0795775  # 2D, Beta=20, rs=1
-
-#############  3D  ######################################
-kF = (9.0*np.pi/4.0)**(1.0/3.0)/rs
-Nf = kF/2.0/np.pi**2
-Bubble = 0.0971916  # 3D, Beta=10, rs=1
-EF = kF**2
-Beta /= EF
-
+Para = param("inlist", Dim, SpinIndex)
+Order = range(0, Para.Order+1)
+TauBin, ExtMomBin, TauBinSize, ExtMomBinSize = [None, ]*4
 Data = {}  # key: (order)
-DataEqT = {}  # key: (order)
-DataW1 = {}
-DataW2 = {}
-TauBin = None
-ExtMomBin = None
-TauBinSize = None
-ExtMomBinSize = None
 
+folder = "./Beta{0}_rs{1}_lambda{2}/".format(
+    int(Para.Beta*Para.EF), Para.Rs, Para.Mass2)
+files = getListOfFiles(folder)
 
 for order in Order:
-    files = os.listdir(folder)
-    Num = 0
-    Norm = 0
-    Data0 = None
-    # if(order == 0):
-    #     FileName = "vertex{0}_pid[0-9]+.dat".format(chan)
-    # else:
-    #     FileName = "vertex{0}_{1}_pid[0-9]+.dat".format(order, chan)
-
-    FileName = "sigma{0}_pid[0-9]+.dat".format(order)
-
+    Norm = 0.0
+    Data[order] = None
     for f in files:
-        if re.match(FileName, f):
+        if re.search("sigma{0}_pid[0-9]+.dat".format(order), f):
             print "Loading ", f
-            with open(folder+f, "r") as file:
-                line0 = file.readline()
-                Step = int(line0.split(":")[-1])/1000000
-                # print "Step:", Step
-                line1 = file.readline()
-                # print line1
-                Norm += float(line1.split(":")[-1])
-                line3 = file.readline()
-                if TauBin is None:
-                    TauBin = np.fromstring(line3.split(":")[1], sep=' ')
-                    TauBinSize = len(TauBin)
-                line4 = file.readline()
-                if ExtMomBin is None:
-                    ExtMomBin = np.fromstring(line4.split(":")[1], sep=' ')
-                    ExtMomBinSize = len(ExtMomBin)
-                    ExtMomBin /= kF
-            Num += 1
-            d = np.loadtxt(folder+f)
-            if Data0 is None:
-                Data0 = d
+            with open(f, "r") as file:
+                Step = int(file.readline().split(":")[-1])/1000000
+                Norm += float(file.readline().split(":")[-1])
+                TauBin = np.fromstring(file.readline().split(":")[1], sep=' ')
+                TauBinSize = len(TauBin)
+                ExtMomBin = np.fromstring(
+                    file.readline().split(":")[1], sep=' ')
+                ExtMomBinSize = len(ExtMomBin)
+            d = np.loadtxt(f)
+            if Data[order] is None:
+                Data[order] = d
             else:
-                Data0 += d
-    Data0 /= Norm
-    Data0 = Data0.reshape((TauBinSize, ExtMomBinSize))
-
-    # average the angle distribution
-    Data[(order)] = Data0
-
-# print DataEqT[(1)]
-
-
-# def Fourier(f, t):
-
-
-def ErrorPlot(p, x, d, color, marker, label=None, size=4, shift=False):
-    p.plot(x, d, marker=marker, c=color, label=label,
-           lw=1, markeredgecolor="None", linestyle="--", markersize=size)
-
-
-w = 1-0.429
+                Data[order] += d
+    Data[order] /= Norm
+    Data[order] = Data[order].reshape((TauBinSize, ExtMomBinSize))
 
 fig, ax = plt.subplots()
-# ax=fig.add_axes()
-# ax = fig.add_subplot(122)
-
-# plt.subplot(1,2,2)
-ColorList = ['k', 'r', 'b', 'g', 'm', 'c', 'navy', 'y']
-ColorList = ColorList*40
-
 
 if(XType == "Mom"):
     for o in Order:
@@ -148,15 +59,17 @@ if(XType == "Mom"):
     ax.set_xlim([ExtMomBin[0], ExtMomBin[-1]])
     ax.set_xlabel("$Ext K$", size=size)
 
-    x = ExtMomBin*kF
-    l = Mass2+Lambda
+    x = ExtMomBin
+    l = Para.Mass2+Para.Lambda
+    kF = Para.kF
     y = 2.0*kF/np.pi*(1.0+l/kF*np.arctan((x-kF)/l)-l/kF*np.arctan((x+kF)/l) -
                       (l*l-x*x+kF*kF)/4.0/x/kF*np.log((l*l+(x-kF)**2)/(l*l+(x+kF)**2)))
     ErrorPlot(ax, ExtMomBin, y, "k", ".", "Analytic")
+
 elif(XType == "Z"):
-    for o in Order:
-        ErrorPlot(ax, ExtMomBin, (DataW1[o]-DataW2[o])/(2.0*np.pi/Beta),
-                  ColorList[o], 's', "Order {0}".format(o))
+    # for o in Order:
+    #     ErrorPlot(ax, ExtMomBin, (DataW1[o]-DataW2[o])/(2.0*np.pi/Beta),
+    #               ColorList[o], 's', "Order {0}".format(o))
     ax.set_xlim([ExtMomBin[0], ExtMomBin[-1]])
     ax.set_xlabel("$Ext K$", size=size)
 
@@ -165,9 +78,9 @@ elif(XType == "Tau"):
     o = 2
     for i in range(N):
         q = i*ExtMomBinSize/N
-        ErrorPlot(ax, TauBin/Beta, Data[o][:, q], ColorList[i],
+        ErrorPlot(ax, TauBin/Para.Beta, Data[o][:, q], ColorList[i],
                   's', "k={0}".format(ExtMomBin[q]))
-        ax.set_xlim([TauBin[0]/Beta, TauBin[-1]/Beta])
+        ax.set_xlim([TauBin[0]/Para.Beta, TauBin[-1]/Para.Beta])
 
     # ax.set_xticks([0.0,0.04,0.08,0.12])
     # ax.set_yticks([0.35,0.4,0.45,0.5])
--- utility.py
+++ utility.py
@@ -1,19 +1,65 @@
 import os
+import sys
+import numpy as np
 
 '''
 For the given path, get the List of all files in the directory tree 
 '''
+
+
 def getListOfFiles(dirName):
     listOfFiles = list()
     for (dirpath, dirnames, filenames) in os.walk(dirName):
         listOfFiles += [os.path.join(dirpath, file) for file in filenames]
     return listOfFiles
 
+
+class param:
+    Order, Beta, Rs, Mass2, Lambda, Charge2, TotalStep = [None, ]*7
+    kF, Nf, EF, Bubble = [0.0, ]*4
+
+    def __init__(self, infile, Dim, SpinIndex):
+
+        with open(infile, "r") as file:
+            para = file.readline().split(" ")
+            self.Order = int(para[0])
+            self.Beta = float(para[1])
+            self.Rs = float(para[2])
+            self.Mass2 = float(para[3])
+            self.Lambda = float(para[4])
+            self.Charge2 = float(para[5])
+            self.TotalStep = float(para[6])
+
+        if Dim == 3:
+            self.kF = (9.0*np.pi/4.0)**(1.0/3.0)/self.Rs
+            self.Nf = self.kF/4.0/np.pi**2*SpinIndex
+        elif Dim == 2:
+            self.kF = np.sqrt(2.0)/self.Rs  # 2D
+            print "Not Implemented for Dimension {0}".format(Dim)
+            sys.exit(0)
+        else:
+            print "Not Implemented for Dimension {0}".format(Dim)
+            sys.exit(0)
+
+        self.EF = self.kF**2
+        self.Beta /= self.EF
+
+
+def ErrorPlot(p, x, d, color, marker, label=None, size=4, shift=False):
+    p.plot(x, d, marker=marker, c=color, label=label,
+           lw=1, markeredgecolor="None", linestyle="--", markersize=size)
+
+
+ColorList = ['k', 'r', 'b', 'g', 'm', 'c', 'navy', 'y']
+ColorList = ColorList*40
+
+
 def main():
-    dirName="./Beta40_rs4.0_lambda0.2301"
+    dirName = "./Beta40_rs4.0_lambda0.2301"
 
     for elem in getListOfFiles(dirName):
-        print(elem)    
-                
+        print(elem)
+
+
 if __name__ == '__main__':
     main()
