1223d14df74a0c4344bf9b93761fa8ac041bfa38
Kun Chen <chenkun@mail.ustc.edu.cn>
2020-05-15 11:04:56 -0400

    clean up

--- delta.py
+++ delta.py
@@ -6,12 +6,12 @@ XType = "Tau"
 # XType = "Mom"
 
 Para = param()
+Grid = grid(Para)
 Order = range(0, Para.Order+1)
 TauGrid = BuildTauGrid(Para.Beta, TauGridSize)
 MomGrid = BuildMomGrid(Para.MaxExtMom, MomGridSize)
 
-folder = "./Beta{0}_rs{1}_lambda{2}/".format(
-    int(Para.Beta*Para.EF), Para.Rs, Para.Mass2)
+folder = "./Data"
 
 filename = "delta_pid[0-9]+.dat"
 
